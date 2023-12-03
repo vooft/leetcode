0bffc5fa0a78962f4a63c1206010f9b5f18b8083
Nikolay Mikheev <el.vooft@gmail.com>
2023-12-03 17:26:10 +0000

    Cleanup and renamed after kotlin migration

--- src/main/kotlin/P009_PalindromeNumber.kt
+++ src/main/kotlin/P009_PalindromeNumber.kt
@@ -4,24 +4,25 @@ import java.util.LinkedList
 /**
  * https://leetcode.com/problems/palindrome-number/
  *
- * Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+ * Determine whether an integer is a palindrome.
+ * An integer is a palindrome when it reads the same backward as forward.
  *
  * Example 1:
- *
  * Input: 121
  * Output: true
- * Example 2:
  *
+ * Example 2:
  * Input: -121
  * Output: false
- * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
- * Example 3:
+ * Explanation: From left to right, it reads -121. From right to left, it becomes 121-.
+ * Therefore it is not a palindrome.
  *
+ * Example 3:
  * Input: 10
  * Output: false
  * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
- * Follow up:
  *
+ * Follow up:
  * Could you solve it without converting the integer to a string?
  */
 class P009_PalindromeNumber {
